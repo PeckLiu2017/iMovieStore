d1ee177af5bd2eb745fc95a19fb53c40a4047b23
peck liu <xiaoyang20170204@gmail.com>
2017-03-24 21:38:40 +0800

    implement movies#edit update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,19 @@ class MoviesController < ApplicationController
     redirect_to movies_path,alert:"Movie Deleted"
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path,notice:"Movie Updated!"
+    else
+      render :edit
+    end
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
