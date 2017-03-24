946f68749acba56a68e7d3943b333f0d0f3955da
peck liu <xiaoyang20170204@gmail.com>
2017-03-24 21:13:31 +0800

    implement movies#new create destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,31 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+     @movie.save
+      redirect_to movies_path,notice:"Movie Added!"
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path,alert:"Movie Deleted"
+  end
+
+  def show
+    @movies = Movie.find(params[:id])
+  end
+  
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title,:description)
+  end
 end
