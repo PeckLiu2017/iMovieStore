a68fcfe8002e3b0209274567d6f880fba89106cf
peck liu <xiaoyang20170204@gmail.com>
2017-03-25 16:42:04 +0800

    implement movie relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
   validates :title, presence: true
 
   has_many :reviews
+  has_many :movie_relationships
+  has_many :members, through: :movie_relationships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :movies
   has_many :reviews
+
+  has_many :movie_relationships
+  has_many :reviews_movies,:through => :movie_relationships, :source => :movie
+
 end
